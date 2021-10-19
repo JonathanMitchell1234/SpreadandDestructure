535051ca84a2ae27016e952886bc9d9600a44084
Jonathan Mitchell <jonmitchell1234@gmail.com>
2021-10-19 10:23:37 -0400

    Upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let age = 20;
+let canDrive = (age >= 16) ? true : false;
+console.log(canDrive);
+
+
+let password = "password";
+let passwordCheck = (password.length >= 11) ? "Meets Length Requirement" : "Invalid. Does NOT Meet Length Requirement";
+console.log(passwordCheck);
+
+
+(true) ? console.log("It was true!") : console.log("It was false!");
+
+// Example 3 (Using IF ELSE)
+if (true) {
+  console.log("It was True!")
+} else {
+  console.log("It was False!")
+}
